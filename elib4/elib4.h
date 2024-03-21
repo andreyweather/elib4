f3cffe8c7998a972dca14ee3cfd501a662f1c527
andreyPogodin <andreyweather@gmail.com>
2024-03-21 12:53:05 +0300

    commit

---
+++ elib4/elib4.h
@@ -0,0 +1,18 @@
+//
+//  elib4.h
+//  elib4
+//
+//  Created by andrey.pogodin on 21.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for elib4.
+FOUNDATION_EXPORT double elib4VersionNumber;
+
+//! Project version string for elib4.
+FOUNDATION_EXPORT const unsigned char elib4VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <elib4/PublicHeader.h>
+
+
